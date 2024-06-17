2a34b4e4f70506e7d2c6d3085d91ca45accf9352
Daniel Bauer <dan717@gmx.de>
2024-06-17 18:31:11 +0200

    Positioning Mesh
    The cubes are stored in a Cube component and referenced in the app component.
    
    Additionally worked with indirect light.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
+
 // Importiert die notwendigen Komponenten von react-three-fiber
 import { Canvas } from '@react-three/fiber';
 import './App.css';
+
+// Cube Komponente:
+const Cube = ({ position, size, color }) => {
+	return (
+		// Erstellt eine 3D-Szene
+		<mesh position={position}>
+			{/* Erstellt ein 3D-Objekt */}
+			{/* Hier wird ein Würfel erstellt */}
+			<boxGeometry args={size} />
+			{/* Hier wird dem Würfel ein Material hinzugefügt */}
+			<meshStandardMaterial color={color} />
+		</mesh>
+	);
+};
 
 // Definiert eine React-Komponente
 const App = () => {
 	return (
 		// Erstellt ein 3D-Canvas. Hier wird die 3D-Szene gerendert.
 		<Canvas>
-			{/* Hier fügen werde Szene eine Beleuchtung hinzu */}
+			{/* Hier fügen werde Szene eine direkte Beleuchtung hinzu */}
+			<directionalLight position={[0, 0, 2]} intensity={1} />
 
-			<directionalLight position={[0, 0, 2]} />
+			{/* Hier fügen werde Szene eine indirekte Beleuchtung hinzu */}
+			<ambientLight intensity={1} />
 
-			{/* Erstellt eine 3D-Szene */}
-			<mesh>
-				{/* Fügt ein 3D-Objekt hinzu, in diesem Fall eine Box */}
-				{/* Hier wird eine Box mit den Dimensionen 2, 2, 4 erstellt */}
-				{/* <boxGeometry args={[2, 2, 4]} /> */}
-				<boxGeometry />
-				{/* Fügt ein Material hinzu. Standard Material braucht aber eine Beleuchtung. */}
-				<meshStandardMaterial color={'orange'} />
-				{/* Im Gegensatz dazu braucht Basic Material keine Beleuchtung. */}
-				{/* <meshBasicMaterial /> */}
-			</mesh>
+			{/* Hier werden die Würfel hinzugefügt */}
+			<Cube position={[-1, 1, 0]} size={[1, 1, 1]} color={'green'} />
+			<Cube position={[1, 1, 0]} size={[1, 1, 1]} color={'hotpink'} />
+			<Cube position={[-1, -1, 0]} size={[1, 1, 1]} color={'orange'} />
+			<Cube position={[1, -1, 0]} size={[1, 1, 1]} color={'blue'} />
 		</Canvas>
 	);
 };
