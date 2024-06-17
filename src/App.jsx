f9b477059ae3481e6a138310f86302675d5c1694
Daniel Bauer <dan717@gmx.de>
2024-06-17 16:08:29 +0200

    Creating a Box Learned about the material and size of the 3-D object box

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 // Importiert die notwendigen Komponenten von react-three-fiber
 import { Canvas } from '@react-three/fiber';
 import './App.css';
@@ -9,7 +10,13 @@ const App = () => {
 			{/* Erstellt eine 3D-Szene */}
 			<mesh>
 				{/* Fügt ein 3D-Objekt hinzu, in diesem Fall eine Box */}
+				{/* Hier wird eine Box mit den Dimensionen 2, 2, 4 erstellt */}
+				{/* <boxGeometry args={[2, 2, 4]} /> */}
 				<boxGeometry />
+				{/* Fügt ein Material hinzu. Standard Material braucht aber eine Beleuchtung. */}
+				<meshStandardMaterial />
+				{/* Im Gegensatz dazu braucht Basic Material keine Beleuchtung. */}
+				{/* <meshBasicMaterial /> */}
 			</mesh>
 		</Canvas>
 	);
