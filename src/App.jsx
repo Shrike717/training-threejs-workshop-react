d786808b95ba1020c85004f47b11f002b78aaeb0
Daniel Bauer <dan717@gmx.de>
2024-06-18 10:40:17 +0200

    Interactions We have learned to create interactions such as hovering or clicking on the object. This allows us to influence the appearance and behavior of the object.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
 // Importiert die notwendigen Komponenten von react-three-fiber
 import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 // Cube Komponente:
 const Cube = ({ position, size, color }) => {
@@ -40,26 +40,49 @@ const Sphere = ({ position, args, color }) => {
 	// Das ist der Hook useRef, den wir brauchen, um unser Element im DOM targetieren zu können:
 	const ref = useRef();
 
+	// Das ist ein State, den wir für die Hover Interaktion brauchen:
+	const [isHovered, setIsHovered] = useState(false);
+
+	// Ist ein Staat, den wir für eine Klick Interaktion brauchen:
+	const [isClicked, setIsClicked] = useState(false);
+
 	// Das ist der Hook useFrame, den wir zum animieren brauchen:
 	useFrame((state, delta) => {
 		// Hier wird die Animation definiert
+
+		// Hier wird die Hover Interaktion definiert. Wenn der Sphere gehovert wird, dann wird die Geschwindigkeit auf 1 gesetzt, ansonsten auf 0.4
+		const speed = isHovered ? 1 : 0.4;
+
 		// Wir rotieren den Sphere
-		ref.current.rotation.x += delta;
-		ref.current.rotation.y += delta * 2;
+		// ref.current.rotation.x += delta;
+		ref.current.rotation.y -= delta * speed;
 		// Hier bewegen wir den Sphere:
 		// ref.current.position.z += delta; // Bewegt den Sphere in Richtung der Z-Achse und eer veschwindet aus dem Sichtfeld
-		ref.current.position.z = Math.sin(state.clock.getElapsedTime()) * 2; // Bewegt den Sphere in Richtung der Z-Achse und wieder zurück
+		// ref.current.position.z = Math.sin(state.clock.getElapsedTime()) * 2; // Bewegt den Sphere in Richtung der Z-Achse und wieder zurück
+
 		// console.log(state);
 	});
 
 	return (
 		// Erstellt eine 3D-Szene
-		<mesh position={position} ref={ref}>
+		<mesh
+			position={position}
+			ref={ref}
+			onPointerEnter={(event) => (
+				event.stopPropagation(), setIsHovered(true)
+			)} // Wenn der Sphere gehovert wird, dann wird der State isHovered auf true gesetzt. stopPropagation() verhindert, dass das Event an die Elternkomponente weitergegeben wird.
+			onPointerLeave={() => setIsHovered(false)} // Wenn der Sphere nicht mehr gehovert wird, dann wird der State isHovered auf false gesetzt
+			onClick={() => setIsClicked(!isClicked)} // Wenn der Sphere geklickt wird, dann wird der State isClicked auf true gesetzt
+			scale={isClicked ? 1.5 : 1} // Wenn der Sphere geklickt wird, dann wird der Sphere skaliert
+		>
 			{/* Erstellt ein 3D-Objekt */}
 			{/* Hier wird ein Sphere erstellt */}
 			<sphereGeometry args={args} />
 			{/* Hier wird dem Sphere ein Material hinzugefügt */}
-			<meshStandardMaterial color={color} wireframe />
+			<meshStandardMaterial
+				color={isHovered ? 'lightblue' : 'orange'}
+				wireframe
+			/>
 		</mesh>
 	);
 };
@@ -70,16 +93,16 @@ const Torus = ({ position, args, color }) => {
 	const ref = useRef();
 
 	// Das ist der Hook useFrame, den wir zum animieren brauchen:
-	useFrame((state, delta) => {
-		// Hier wird die Animation definiert
-		// Wir rotieren den Sphere
-		ref.current.rotation.x += delta;
-		ref.current.rotation.y += delta * 2;
-		// Hier bewegen wir den Sphere:
-		// ref.current.position.z += delta; // Bewegt den Sphere in Richtung der Z-Achse und eer veschwindet aus dem Sichtfeld
-		ref.current.position.z = Math.sin(state.clock.getElapsedTime()) * 2; // Bewegt den Sphere in Richtung der Z-Achse und wieder zurück
-		// console.log(state);
-	});
+	// useFrame((state, delta) => {
+	// 	// Hier wird die Animation definiert
+	// 	// Wir rotieren den Sphere
+	// 	ref.current.rotation.x += delta;
+	// 	ref.current.rotation.y += delta * 2;
+	// 	// Hier bewegen wir den Sphere:
+	// 	// ref.current.position.z += delta; // Bewegt den Sphere in Richtung der Z-Achse und eer veschwindet aus dem Sichtfeld
+	// 	ref.current.position.z = Math.sin(state.clock.getElapsedTime()) * 2; // Bewegt den Sphere in Richtung der Z-Achse und wieder zurück
+	// 	// console.log(state);
+	// });
 
 	return (
 		// Erstellt eine 3D-Szene
@@ -147,21 +170,21 @@ const App = () => {
 			{/* <Cube position={[0, 0, 0]} size={(1, 1, 1)} color={'orange'} /> */}
 
 			{/* Hier wird die Sphere hinzugefügt */}
-			<Sphere position={[0, 0, 0]} args={[1, 30, 30]} color={'orange'} />
+			<Sphere position={[0, 0, 0]} args={[2, 30, 30]} color={'orange'} />
 
 			{/* Hier wird der Torus hinzugefügt */}
-			<Torus
+			{/* <Torus
 				position={[2.2, 0, 0]} // Position des Torus
 				args={[0.8, 0.2, 30, 30]} // Argumente für den Torus: [Radius, Rohrdurchmesser, radiale Segmente, tubulare Segmente]
 				color={'blue'} // Farbe des Torus
-			/>
+			/> */}
 
 			{/* Hier wird der TorusKnot hinzugefügt */}
-			<TorusKnot
+			{/* <TorusKnot
 				position={[-2.4, 0, 0]} // Position des TorusKnot
 				args={[0.6, 0.15, 1000, 50]} // Argumente für den TorusKnot: [Radius, Rohrdurchmesser, radiale Segmente, tubulare Segmente]
 				color={'hotpink'} // Farbe des TorusKnot
-			/>
+			/> */}
 		</Canvas>
 	);
 };
