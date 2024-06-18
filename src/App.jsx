88f1e75394190761df880ed1890ca3f8c4dfc7c0
Daniel Bauer <dan717@gmx.de>
2024-06-18 09:00:08 +0200

    Animation Learned how to animate an object. Rotation around the axes as well as a sine movement along the Z axis

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@
 /* eslint-disable react/no-unknown-property */
 
 // Importiert die notwendigen Komponenten von react-three-fiber
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css';
+import { useRef } from 'react';
 
 // Cube Komponente:
 const Cube = ({ position, size, color }) => {
+	// Das ist der Hook useRef, den wir brauchen, um unser Element im DOM targetieren zu können:
+	const ref = useRef();
+
+	// Das ist der Hook useFrame, den wir zum animieren brauchen:
+	useFrame((state, delta) => {
+		// Hier wird die Animation definiert
+		// Wir rotieren den Cube
+		ref.current.rotation.x += delta;
+		ref.current.rotation.y += delta * 2;
+		// Hier bewegen wir den Cube:
+		// ref.current.position.z += delta; // Bewegt den Cube in Richtung der Z-Achse und eer veschwindet aus dem Sichtfeld
+		ref.current.position.z = Math.sin(state.clock.getElapsedTime()) * 2; // Bewegt den Cube in Richtung der Z-Achse und wieder zurück
+		// console.log(state);
+	});
+
 	return (
 		// Erstellt eine 3D-Szene
-		<mesh position={position}>
+		<mesh position={position} ref={ref}>
 			{/* Erstellt ein 3D-Objekt */}
 			{/* Hier wird ein Würfel erstellt */}
-			<boxGeometry args={size} />
+			<boxGeometry args={[size, size, size]} />
 			{/* Hier wird dem Würfel ein Material hinzugefügt */}
 			<meshStandardMaterial color={color} />
 		</mesh>
@@ -28,10 +44,10 @@ const App = () => {
 			<directionalLight position={[0, 0, 2]} intensity={1} />
 
 			{/* Hier fügen werde Szene eine indirekte Beleuchtung hinzu */}
-			<ambientLight intensity={1} />
+			<ambientLight intensity={0.7} />
 
 			{/* Hier werden die Würfel hinzugefügt */}
-			<group position={[0, 1.5, 0]}>
+			{/* <group position={[0, 0, 0]}>
 				<Cube position={[-1, 1, 0]} size={[1, 1, 1]} color={'green'} />
 				<Cube position={[1, 1, 0]} size={[1, 1, 1]} color={'hotpink'} />
 				<Cube
@@ -40,7 +56,10 @@ const App = () => {
 					color={'orange'}
 				/>
 				<Cube position={[1, -1, 0]} size={[1, 1, 1]} color={'blue'} />
-			</group>
+			</group> */}
+
+			{/* Hier wird ein animierter Würfel erstellt */}
+			<Cube position={[0, 0, 0]} size={(1, 1, 1)} color={'orange'} />
 		</Canvas>
 	);
 };
